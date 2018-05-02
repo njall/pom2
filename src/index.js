f117057db37487c7461089665311a19781ab36dc
Niall Beard <niallbeard@gmail.com>
2018-05-02 23:31:33 +0100

    Added film selector, found film search, refactored

--- src/index.js
+++ src/index.js
@@ -3,15 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 var config = require('./config.json')
 
-function Film(props){
+
+class Film extends React.Component{
+    render(props){
+        console.log(this.props)
       return (
-        <div className="Film">
-            <div className="film-title">Title: {props.film.Title}</div>
-            <img className="film-poster" src={props.film.Poster}/>
+        <div className="film">
+            <div className="film-title">{this.props.film.Title}</div>
+            <img className="film-poster" alt={this.props.film.Title} src={this.props.film.Poster}/>
         </div>
       )
+    }
 }
 
+
+class FilmList extends React.Component{
+
+    render(props){
+        var suggestedFilms = [];
+        console.log(this.props)
+        this.props.films.map((film,i)=>{return suggestedFilms.push(
+            <button key={i} onClick={this.props.loadFilmMethod.bind(this, film.imdbID)}>
+                <div className="film">
+                    <div className="film-title">{film.Title}</div>
+                    <img className="film-poster" alt={film.Title} src={film.Poster}/>
+                </div>
+            </button>
+        )})
+        return (
+            <div className="suggestions">
+                <div>Which <em>{this.props.query}</em> did you mean?</div><br/>
+                <div className="filmList">    
+                    {suggestedFilms}
+                </div>
+            </div>
+        )
+    }
+}
 
 class Search extends React.Component{
     constructor(props){
@@ -19,49 +47,66 @@ class Search extends React.Component{
         this.state = {
             inputValue: '',
             searched: false,
-            /* Film Stuffz */
-            film: {}
+            suggestedFilms: [],
+            showResults: false,
+            foundFilm: {}, 
+            found: false        
         }
     }
 
-    renderResults(){
-        console.log(this.state)
-        const { error, searched, film } = this.state;
-        if (!searched) {
-          return <div>Type in a film to begin</div>;
-        } else if (error) {
-          return <div>Error: {error.message}</div>;
-        } else if (film === null || film === {}) {
-            return <div> Film Not Found</div>
-        } else {
-            return <Film film={this.state.film}/>
-        }        
+    loadFilm(id){
+        var queryEndpoint = 'http://www.omdbapi.com/'
+            + '?apikey=' + config.apiKey 
+            + '&i=' + id
+            + '&type=movie' 
+            + '&r=json'
+            /*console.log(queryEndpoint)*/
+        fetch(queryEndpoint)
+            .then(res => res.json())
+            .then(
+                (result) => {       
+                    console.log(result)
+                    this.setState({
+                        showResults: false,
+                        suggestedFilms: [],
+                        searched: false,
+                        foundFilm: result,
+                        found: true
+                    })      
+                }
+            )
+            
     }
-
-    findFilm(evt){
+    
+    findFilms(evt){
         var queryEndpoint = 'http://www.omdbapi.com/'
             + '?apikey=' + config.apiKey 
             + '&s=' + this.state.inputValue 
             + '&type=movie' 
             + '&r=json'
-            console.log(queryEndpoint)
+            /*console.log(queryEndpoint)*/
         fetch(queryEndpoint)
             .then(res => res.json())
             .then(
                 (result) => {
-                    var film;
-                    if (result.Search !== undefined
-                      && Array.isArray(result.Search)
-                      && result.Search.length > 0){
-                        film = result.Search[0]
-                    } else {
-                        film = null
+                    var suggestedFilms = [];
+                    var showResults = false;
+                    if (result.Search !== undefined && Array.isArray(result.Search)){
+                        if (result.Search.length > 1){
+                            suggestedFilms = result.Search.slice(0,5)                            
+                            showResults = true;
+                            this.setState({
+                                searched: true,
+                                showResults: showResults,
+                                suggestedFilms: suggestedFilms,
+                                foundFilm: {},
+                                found: false
+                            })
+                        } else if (result.Search.length === 1){
+                            this.loadFilm(result.Search[0].imdbID)
+                        }
+
                     }
-                    console.log(film)
-                    this.setState({
-                        searched: true,
-                        film: film
-                    })
                 },
                 (error) => {
                     this.setState({
@@ -73,6 +118,7 @@ class Search extends React.Component{
     }
 
     render(){
+        console.log(this.state.film)
       return (
         <div>
             <input
@@ -80,9 +126,16 @@ class Search extends React.Component{
                 value={this.state.inputValue} 
                 onChange={evt=>this.updateInputValue(evt)}
              />
-            <button className="search" onClick={()=>this.findFilm()}>Go!</button>
+            <button className="search" onClick={()=>this.findFilms()}>Go!</button>
             
-            {this.renderResults()}
+            {this.state.showResults ? <FilmList 
+                                            loadFilmMethod={this.loadFilm.bind(this)}
+                                            films={this.state.suggestedFilms} 
+                                            query={this.state.inputValue}/> : null}
+            {(this.state.suggestedFilms.length === 0 && this.state.searched) ? 
+                <div>None found</div> : null}
+            {this.state.found ? 
+                            <Film film={this.state.foundFilm} /> : null}                
         </div>
       )
   }
@@ -95,15 +148,12 @@ class Search extends React.Component{
 }
 
 function App(){
-
-        console.log(config)
   return (
-
     <div>
         <h1>Pretentious-o-Meter 2.0</h1>
         <Search /> 
     </div>
-    )   
+  )   
 }
 
 
